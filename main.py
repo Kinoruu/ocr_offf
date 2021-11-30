80b78849316fff4c28dfdffd0d8b3ad365b18a59
Jakub Siejak <jakubsiejak@gmail.com>
2021-11-30 08:00:57 +0100

    cleaned some code
    TO DO:
    - also add next datasets as special symbols and japanese character
    - normalize input to smaller datasets f.e. 400, 500, 600, 700, 800

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import sys
 import pandas as pd
 import cv2
 import time
+import argparse
+import matplotlib.font_manager as fm
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
@@ -25,35 +27,18 @@ from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
-import matplotlib.font_manager as fm
 from matplotlib.collections import QuadMesh
 from pandas import DataFrame
-import argparse
+from tensorflow.keras import regularizers
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
 (X_train_1, Y_train_1), (X_test_1, Y_test_1) = keras.datasets.mnist.load_data()
 data = pd.read_csv("A_Z Handwritten Data.csv").astype('float32')
 
-# print(y_train.shape)
-
-# print(len(X_train))
-# print(len(X_test))
-
-# print(X_train[0].shape)
-
-# print(X_train[0])
-# plt.show(X_train[0])
-
-
-# print(img_train.shape)
-# print(label_train.shape)
-# print(img_train[0].shape)
-# print(label_train.shape)
-# print(img_train[0])
-# print(label_train[0])
-
 # final try
+
 
 def load_az_dataset():
     # initialize the list of data and labels
@@ -62,7 +47,7 @@ def load_az_dataset():
     data_az = pd.read_csv("A_Z Handwritten Data.csv").astype('float32')
     data_az.rename(columns={'0': 'label'}, inplace=True)
 
-    # Splite data the X - Our data , and y - the prdict label
+    # Splite data the X - Our data , and y - the predict label
     x = data_az.drop('label', axis=1)
     y = data_az['label']
     data_az_tr, data_az_ts, labels_az_tr, labels_az_ts = train_test_split(x, y)
@@ -75,17 +60,9 @@ def load_az_dataset():
 
     data_az_tr = data_az_tr.reshape(data_az_tr.shape[0], 28, 28, 1).astype('float32')  # zmiana kształtu na 3 wymiary
     data_az_ts = data_az_ts.reshape(data_az_ts.shape[0], 28, 28, 1).astype('float32')  # zmiana kształtu na 3 wymiary
-    # print(labels_az_tr[0])
-    # labels_az_tr = np_utils.to_categorical(labels_az_tr)
-    # labels_az_ts = np_utils.to_categorical(labels_az_ts)
-    # print(labels_az_tr[0])
+
     labels_az_tr = np.array(labels_az_tr).astype(int)
     labels_az_ts = np.array(labels_az_ts).astype(int)
-    # print(labels_az_tr[0])
-    # print(labels_az_tr[1])
-    # print(labels_az_tr[2])
-    # print(labels_az_tr[3])
-    # print(labels_az_tr[0])
 
     # return a 2-tuple of the A-Z data and labels
     return data_az_tr, labels_az_tr, data_az_ts, labels_az_ts
@@ -98,7 +75,6 @@ def load_zero_nine_dataset():
     ((train_data, train_labels), (test_data, test_labels)) = mnist.load_data()
     data = np.vstack([train_data, test_data])
     labels = np.hstack([train_labels, test_labels])
-    # print(train_labels[0])
     train_data = train_data.reshape(train_data.shape[0], 28, 28, 1).astype('float32')
     test_data = test_data.reshape(test_data.shape[0], 28, 28, 1).astype('float32')
 
@@ -108,12 +84,9 @@ def load_zero_nine_dataset():
 # load all datasets
 (az_data_tr, az_labels_tr, az_data_ts, az_labels_ts) = load_az_dataset()
 (digits_data_tr, digits_labels_tr, digits_data_ts, digits_labels_ts) = load_zero_nine_dataset()
-# print(digits_data_tr.shape)
-#digits_labels_tr
-#digits_data_ts
-#digits_labels_ts
 
-# the MNIST dataset occupies the labels 0-9, so let's add 10 to every A-Z label to ensure the A-Z characters are not incorrectly labeled as digits
+# the MNIST dataset occupies the labels 0-9,
+# so let's add 10 to every A-Z label to ensure the A-Z characters are not incorrectly labeled as digits
 az_labels_tr += 10
 az_labels_ts += 10
 
@@ -123,7 +96,7 @@ labels_tr = np.hstack([az_labels_tr, digits_labels_tr])
 labels_ts = np.hstack([az_labels_ts, digits_labels_ts])
 
 
-def train_my_model(optm, num, epochs):
+def train_my_model(optm, num, epochs):  # train_my_model(optimizer, number for image saving, number of epochs)
     '''
     model_ld = Sequential()
     model_ld.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
@@ -138,55 +111,54 @@ def train_my_model(optm, num, epochs):
     model_ld.add(Dense(36, activation="softmax"))
     '''
 
-    model_ld = Sequential()
+    model_ld = Sequential()  # LeNet-5 upgraded
     # Layer 1
-    model_ld.add(Conv2D(filters=32, kernel_size=5, strides=1, activation= "relu", input_shape = (28, 28, 1)))
+    model_ld.add(Conv2D(filters=32, kernel_size=5, strides=1, activation="relu", input_shape=(28, 28, 1),
+                        kernel_regularizer=regularizers.l2(0.0005)))
     # Layer 2
-    model_ld.add(Conv2D(filters=32, kernel_size=5, strides=1, use_bias=False, activation= "relu"))
+    model_ld.add(Conv2D(filters=32, kernel_size=5, strides=1, use_bias=False, activation="relu"))
     # Layer 3
     model_ld.add(BatchNormalization())
     model_ld.add(MaxPooling2D(pool_size=2, strides=2))
     model_ld.add(Dropout(0.25))
     # Layer 3
-    model_ld.add(Conv2D(filters=64, kernel_size=3, strides=1, activation= "relu"))
+    model_ld.add(Conv2D(filters=64, kernel_size=3, strides=1, activation="relu",
+                        kernel_regularizer=regularizers.l2(0.0005)))
     # Layer 4
-    model_ld.add(Conv2D(filters=64, kernel_size=3, strides=1, use_bias=False, activation= "relu"))
+    model_ld.add(Conv2D(filters=64, kernel_size=3, strides=1, use_bias=False, activation="relu"))
     # Layer 5
     model_ld.add(BatchNormalization())
     model_ld.add(MaxPooling2D(pool_size=2, strides=2))
     model_ld.add(Dropout(0.25))
     model_ld.add(Flatten())
     # Layer 6
-    model_ld.add(Dense(units=256, use_bias=False, activation= "relu"))
+    model_ld.add(Dense(units=256, use_bias=False, activation="relu"))
     # Layer 7
     model_ld.add(BatchNormalization())
     # Layer 8
-    model_ld.add(Dense(units=128, use_bias=False, activation= "relu"))
+    model_ld.add(Dense(units=128, use_bias=False, activation="relu"))
     # Layer 9
     model_ld.add(BatchNormalization())
     # Layer 10
-    model_ld.add(Dense(units=84, use_bias=False, activation= "relu"))
+    model_ld.add(Dense(units=84, use_bias=False, activation="relu"))
     # Layer 11
     model_ld.add(BatchNormalization())
-    # — — — — — — — — — — — — — — — — #
     model_ld.add(Dropout(0.25))
-    # — — — — — — — — — — — — — — — — #
     # Output
-    model_ld.add(Dense(units=36, activation= "softmax"))
-
+    model_ld.add(Dense(units=36, activation="softmax"))
 
     # optimizer: adam , sgd , rmsprop, SGD(lr=1e-4, momentum=0.9)
     callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
-    model_ld.compile(optimizer=optm, loss='sparse_categorical_crossentropy', metrics=['accuracy'], ) # run_eagerly=True)
+    model_ld.compile(optimizer=optm, loss='sparse_categorical_crossentropy', metrics=['accuracy'])  # run_eagerly=True)
 
-    history = model_ld.fit(data_tr, labels_tr, epochs=epochs, validation_data=(data_ts, labels_ts), callbacks=[callback],
-                           use_multiprocessing=True)
+    history = model_ld.fit(data_tr, labels_tr, epochs=epochs, validation_data=(data_ts, labels_ts), callbacks=[callback]
+                           , use_multiprocessing=True)
 
     model_ld.summary()
 
     model_ld.evaluate(data_ts, labels_ts, use_multiprocessing=True)
 
-    #model_ld.save('CNN_ld_2.model')
+    # model_ld.save('CNN_ld_2.model')
 
     print("The validation accuracy is :", history.history['val_accuracy'])
     print("The training accuracy is :", history.history['accuracy'])
@@ -207,15 +179,18 @@ def train_my_model(optm, num, epochs):
     heat_map = sn.heatmap(c_m, annot=True, fmt='.3f', xticklabels=names, yticklabels=names, cbar=False)
     heat_map.set_yticklabels(heat_map.get_yticklabels(), rotation=35)
     sn.set(font_scale=0.8)
-    plt.title('CNN_ld_' + str(num) + '.model training ' + optm + ' epochs ' + str(epochs) + 'test of LeNet-5 based neural network', fontsize=20)
+    plt.title('CNN_ld_' + str(num) + '.model training ' + optm + ' epochs ' + str(epochs) +
+              'test of LeNet-5 based neural network v2', fontsize=18)
     plt.xlabel('Predicted')
     plt.ylabel('Truth')
-    #plt.show()
-    plt.savefig('CNN_ld_' + str(num) + '.model training heatmap ' + optm + ' epochs ' + str(epochs) + 'test of LeNet-5 based neural network.png')
-    #cv2.waitKey(1)
+    # plt.show()
+    plt.savefig('CNN_ld_' + str(num) + '.model training heatmap ' + optm + ' epochs ' + str(epochs) +
+                'test of LeNet-5 based neural network v2.png')
+    # cv2.waitKey(1)
 
     plt.figure(figsize=(10, 5))
-    plt.suptitle('CNN_ld_' + str(num) + '.model training ' + optm + ' epochs ' + str(epochs) + 'test of LeNet-5 based neural network', fontsize=20)
+    plt.suptitle('CNN_ld_' + str(num) + '.model training ' + optm + ' epochs ' + str(epochs) +
+                 'test of LeNet-5 based neural network v2', fontsize=18)
 
     plt.subplot(1, 2, 1)
     plt.xlabel('Epochs', fontsize=8)
@@ -230,10 +205,11 @@ def train_my_model(optm, num, epochs):
     plt.plot(history.history['accuracy'], label='Training Accuracy')
     plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
     plt.legend(loc='lower right')
-    #plt.show()
-    plt.savefig('CNN_ld_' + str(num) + '.model training evaluation ' + optm + ' epochs ' + str(epochs) + 'test of LeNet-5 based neural network.png')
+    # plt.show()
+    plt.savefig('CNN_ld_' + str(num) + '.model training evaluation ' + optm + ' epochs ' + str(epochs) +
+                'test of LeNet-5 based neural network v2.png')
 
-#train_my_model(optimizer, number for image saving, number of epochs)
+
 '''
 train_my_model('adam', 1, 20)
 train_my_model('adam', 1, 25)
@@ -263,18 +239,19 @@ train_my_model('ftrl', 7, 20)
 train_my_model('ftrl', 7, 25)
 train_my_model('ftrl', 7, 30)
 '''
-#train_my_model('adamax', 5, 35)
-
+# train_my_model('adamax', 5, 35)
 # train_my_model('adamax', 5, 40)
 # train_my_model('adamax', 5, 45)
 # train_my_model('adamax', 5, 50)
 # train_my_model('adamax', 5, 55)
 # train_my_model('adamax', 5, 60)
+
 '''
 for i in range (50):
     train_my_model('adamax',5,i+1)
 '''
+
 start = time.time()
-train_my_model('adamax', 5, 40)
+train_my_model('adamax', 5, 50)
 end = time.time()
-print(end - start)
+print('Time of model learning: ' + str(end - start))
